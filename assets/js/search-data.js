939f75b759b767ba32f3feaa368ad44dfb25ce64
ashutoshsom1 <88818524+ashutoshsom1@users.noreply.github.com>
2025-08-26 18:10:47 +0000

    Deploying to gh-pages from @ ashutoshsom1/ashutoshsom1.github.io@006444e538ef72e0e71e06e782bd4070570743ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Comprehensive overview of my professional journey as an AI/ML Engineer and Data Analyst, specializing in innovative solutions for tabular data analysis, invoice management systems, and Azure Cognitive Services integration.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
